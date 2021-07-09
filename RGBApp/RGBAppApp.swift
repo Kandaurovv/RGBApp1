c6c3f494e1fa2172fbcfb17446f844e03a6caf9b
Kandaurovv <86736067+Kandaurovv@users.noreply.github.com>
2021-07-09 12:34:57 +0300

    Initial Commit

---
+++ RGBApp/RGBAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RGBAppApp.swift
+//  RGBApp
+//
+//  Created by User on 09.07.2021.
+//
+
+import SwiftUI
+
+@main
+struct RGBAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
